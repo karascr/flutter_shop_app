b1133c30504ad97a1b9af85c4a158e8c566c8685
Muhammet Kara <muhammetkaratr@gmail.com>
2022-02-24 00:14:11 +0300

    folder tree reconstructed

--- lib/screens/product/widgets/products_grid.dart
+++ lib/screens/product/widgets/products_grid.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/product.dart';
-import '../providers/products_provider.dart';
+import '../../../providers/product.dart';
+import '../../../providers/products_provider.dart';
 import 'product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
